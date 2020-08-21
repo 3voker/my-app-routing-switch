885459ea223811dd634c80e89dcf0ea32d834394
3voker <Drechristophe@gmail.com>
2020-08-20 23:10:09 -0500

    I hate this so much...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Welcome from "./components/welcome/Welcome";
+//Import the Navigation component
+//import Navigation from "./components/navigation/Navigation";
+
+//Import the Route component
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/clock">Clock</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/welcome/:name">Welcome</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/:id" children={<Child />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+function Child() {
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+    </div>
+  );
+}
 export default App;
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,38 @@
+// import React from "react";
+// import child from "./../../App";
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useParams,
+// } from "react-router-dom";
+
+// //import Link from react-dom-router
+// //import { Link } from "react-router-dom";
+
+// export default function Navigation() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/clock">Clock</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//           <li>
+//             <Link to="/welcome/:name">Welcome</Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// //export default Navigation;
